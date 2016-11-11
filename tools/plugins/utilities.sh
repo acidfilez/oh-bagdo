0e5051062988bb4f5732e83b434d81e6fa0653d8
acidfilez <acidfilez@gmail.com>
2016-11-11 13:02:09 -0300

    bsr

--- tools/plugins/utilities.sh
+++ tools/plugins/utilities.sh
@@ -12,3 +12,5 @@ alias "ducks"="du -cks * | sort -rn | head"
 
 #tails the last file
 alias "tail-last"="tail -f $(ls -1t | head -1)"
+
+alias bsr='browser-sync start --proxy http://bagdo.modyo.me:3000 --browser "Google Chrome" --files "app/assets/stylesheets/**/*.scss, app/assets/javascripts/**/*.js, app/views/**/*.html.erb"'
