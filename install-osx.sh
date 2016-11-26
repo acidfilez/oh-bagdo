a112e5ad97e3d62361168ba163bfb2487fc7c37c
Magno C. Heck <acidfilez@gmail.com>
2016-11-26 11:53:35 -0300

    adding .bagdorc to installation

--- install-osx.sh
+++ install-osx.sh
@@ -2,8 +2,10 @@
 
 ROOT_HOME_PATH="$HOME"
 ROOT_BAGDO_PATH="$ROOT_HOME_PATH/.oh-bagdo"
-ROOT_ZSHRC_FILE="$ROOT_HOME_PATH/.zshrc"
 ROOT_GIT_URL="https://github.com/acidfilez/oh-bagdo.git"
+ROOT_BAGDORC_FILE="$ROOT_HOME_PATH/.bagdorc"
+ROOT_BAGDORC_TEMPLATE_FILE="$ROOT_HOME_PATH/tools/templates/.bagdorc.template"
+ROOT_ZSHRC_FILE="$ROOT_HOME_PATH/.zshrc"
 
 echo -e "\033[38;5;148mTrying to install <Oh-Bagdo/>\033[39m"
 
@@ -12,6 +14,14 @@ if [ -d "$ROOT_BAGDO_PATH" ]; then
 else
   echo -e "\033[38;5;148mFetching, Oh-Bagdo From:\033[39m $ROOT_GIT_URL"
   git clone $ROOT_GIT_URL $ROOT_BAGDO_PATH
+
+  if [ -f "$ROOT_BAGDORC_FILE" ]; then
+    echo -e "\033[38;5;148mSkip, .bagdorc file exist @:\033[39m $ROOT_BAGDORC_FILE"
+  else
+    echo -e "\033[38;5;148mCreating, .bagdorc to:\033[39m $ROOT_BAGDORC_FILE"
+    cp $ROOT_BAGDORC_TEMPLATE_FILE $ROOT_BAGDORC_FILE
+  fi
+
 fi
 
 result=$(grep "Bootstrap Oh-Bagdo" $ROOT_ZSHRC_FILE)
