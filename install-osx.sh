96698d84803932f053843358bc2d34621c817c4c
Magno C. Heck <acidfilez@gmail.com>
2016-11-27 19:30:55 -0300

    badge as a oh-my-zsh plugging, installation

--- install-osx.sh
+++ install-osx.sh
@@ -2,18 +2,31 @@
 
 ROOT_HOME_PATH="$HOME"
 ROOT_BAGDO_PATH="$ROOT_HOME_PATH/.oh-bagdo"
-ROOT_BAGDO_ZSH_PATH="$ZSH_CUSTOM/plugins/oh-bagdo"
+ROOT_BAGDO_ZSH_PATH="$ZSH/custom/plugins/oh-bagdo"
 ROOT_GIT_URL="https://github.com/acidfilez/oh-bagdo.git"
 ROOT_BAGDORC_FILE="$ROOT_HOME_PATH/.bagdorc"
 ROOT_ZSHRC_FILE="$ROOT_HOME_PATH/.zshrc"
 
+INSTALL_TYPE=0
 
 bagdo-install () {
   echo -e "\033[38;5;148m Trying to install <Oh-Bagdo/>\033[39m"
 
   bagdo-clone
-  bagdo-zshrc
-  bagdo-plugin-install
+
+  if [ "$INSTALL_TYPE" -ne 1 ] && [ "$INSTALL_TYPE" -ne 2 ]; then
+    echo -e "\033[38;5;148m Good Bye \033[39m"
+  else
+    if [ "$INSTALL_TYPE" = 1 ]; then
+      bagdo-zshrc
+      bagdo-rc-file-install
+      bagdo-end
+    fi
+    if [ "$INSTALL_TYPE" = 2 ]; then
+      bagdo-rc-file-install
+      bagdo-end
+    fi
+  fi
 }
 
 
@@ -25,10 +38,10 @@ bagdo-clone () {
   echo -e "\033[38;5;148m     1) Install Oh-Bagdo at home: $ROOT_BAGDO_PATH \033[39m"
   echo -e "\033[38;5;148m     2) Install Oh-Bagdo as zsh plugin: $ROOT_BAGDO_ZSH_PATH \033[39m"
 
-  echo "Enter Option (1,2,3):"
-  read line
+  echo "Enter Option (1,2):"
+  read INSTALL_TYPE
 
-  if [ "$line" = 1 ]; then
+  if [ "$INSTALL_TYPE" = 1 ]; then
     if [ -d "$ROOT_BAGDO_PATH" ]; then
       echo -e "\033[38;5;148m Skip, Oh-Bagdo directory exist @:\033[39m $ROOT_BAGDO_PATH"
     else
@@ -37,8 +50,13 @@ bagdo-clone () {
     fi
   fi
 
-  if [ "$line" = 2 ]; then
-    echo 'as a plugin'
+  if [ "$INSTALL_TYPE" = 2 ]; then
+    if [ -d "$ROOT_BAGDO_ZSH_PATH" ]; then
+      echo -e "\033[38;5;148m Skip, Oh-Bagdo directory exist @:\033[39m $ROOT_BAGDO_ZSH_PATH"
+    else
+      echo -e "\033[38;5;148m Fetching, Oh-Bagdo From:\033[39m $ROOT_GIT_URL"
+      git clone $ROOT_GIT_URL $ROOT_BAGDO_ZSH_PATH
+    fi
   fi
 
 }
@@ -51,20 +69,20 @@ if [ ! "$result" = "" ]
   then
     echo -e "\033[38;5;148m Skip, Oh-Bagdo already addedin your:\033[39m $ROOT_ZSHRC_FILE"
   else
-    echo -e "\033[38;5;148m Adding, Oh-Bagdo in your: \033[39m$ROOT_ZSHRC_FILE"
+    echo -e "\033[38;5;148m Modifying File: \033[39m$ROOT_ZSHRC_FILE"
 
 cat <<EOT >> $ROOT_ZSHRC_FILE
 
 #Bootstrap Oh-Bagdo
-export HOME_BAGDO=$HOME/.oh-bagdo
-source $HOME_BAGDO/.bagdorc
+export HOME_BAGDO=\$HOME/.oh-bagdo
+source \$HOME_BAGDO/.bagdorc
 
 EOT
 fi
 
 }
 
-bagdo-plugin-install () { # create bagdo init file#
+bagdo-rc-file-install () { # create bagdo rc file#
 
 if [ -f "$ROOT_BAGDORC_FILE" ]; then
   echo -e "\033[38;5;148m Skip, .bagdorc file exist @:\033[39m $ROOT_BAGDORC_FILE"
@@ -75,37 +93,56 @@ else
 
 cat <<END >$ROOT_BAGDORC_FILE
     #Enable java
-    #source $HOME_BAGDO/tools/plugins/java.sh
+    #source \$HOME_BAGDO/tools/plugins/java.sh
 
     #Enable unix utilites commands
-    #source $HOME_BAGDO/tools/plugins/utilities.sh
+    #source \$HOME_BAGDO/tools/plugins/utilities.sh
 
     #Enable android commands
-    #source $HOME_BAGDO/tools/plugins/android.sh
+    #source \$HOME_BAGDO/tools/plugins/android.sh
 
     #Enable apple commands
-    #source $HOME_BAGDO/tools/plugins/apple.sh
+    #source \$HOME_BAGDO/tools/plugins/apple.sh
 
     #Enable development applications like subl, xcode, etc commands
-    #source $HOME_BAGDO/tools/plugins/development_apps.sh
+    #source \$HOME_BAGDO/tools/plugins/development_apps.sh
 
     #Enable docker commands
-    #source $HOME_BAGDO/tools/plugins/docker.sh
+    #source \$HOME_BAGDO/tools/plugins/docker.sh
 
     #Enable ionic commands
-    #source $HOME_BAGDO/tools/plugins/ionic.sh
+    #source \$HOME_BAGDO/tools/plugins/ionic.sh
 
     #Enable nginix commands, for mac only
-    #source $HOME_BAGDO/tools/plugins/nginx.sh
+    #source \$HOME_BAGDO/tools/plugins/nginx.sh
 
     #Enable ruby commands
-    #source $HOME_BAGDO/tools/plugins/ruby.sh
+    #source \$HOME_BAGDO/tools/plugins/ruby.sh
 END
 
 fi
 
 }
 
-##Start Installing
+bagdo-end () {
+
+echo -e "\033[38;5;148m Oh-Bagdo is ready but before ... \033[39m"
+
+if [ "$INSTALL_TYPE" = 1 ]; then
+  echo -e "\033[38;5;148m Look for the comment '#Bootstrap Oh-Bagdo' in your .zshrc File: \033[39m$ROOT_ZSHRC_FILE"
+  echo -e "\033[38;5;148m finally please do a: source ~/.zshrc\033[39m"
+
+fi
+if [ "$INSTALL_TYPE" = 2 ]; then
+  plugins_installed=$(more $ROOT_ZSHRC_FILE | grep '^plugins.*=.*)')
+  echo -e "\033[38;5;148m Found, oh-my-zsh plugin installed: \033[39m"
+  echo -e "\033[38;5;148m $plugins_installed \033[39m"
+  echo -e "\033[38;5;148m please add 'oh-bagdo' plugin, for example 'plugins=(git oh-bagdo)', edit .zshrc File: \033[39m$ROOT_ZSHRC_FILE"
+  echo -e "\033[38;5;148m finally please do a: source ~/.zshrc\033[39m"
+fi
+
+}
+
+##Main Installation
 
 bagdo-install
